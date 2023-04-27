9f7451c57c2a214fbe78f3b31ba8e6df63bfacef
Joshua Akinola <akinolajsh@gmail.com>
2023-04-27 20:26:28 +0100

    linklist17

---
+++ linklist17.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Linklist17 {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  insert(val) {
+    let node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  remove() {
+    let current = this.tail;
+    let newTail = current;
+    while (current.next) {
+      this.tail = newTail;
+      this.tail = null;
+    }
+    this.length--;
+    return this;
+  }
+}
+
+let linklist17 = new Linklist17();
+
+console.log(linklist17.insert(17));
+console.log(linklist17.insert(170));
+console.log(linklist17.insert(1700));
+linklist17.remove();
+console.log(linklist17.insert(17000));
+console.log(linklist17.insert(17000));
